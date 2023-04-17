1d9efa3cd98a69f1c083e3da2e6985eb537d2502
gya-nyang <104287343+gya-nyang@users.noreply.github.com>
2023-04-17 10:49:55 +0900

    header script update
    header script update

--- adm/js/common.js
+++ adm/js/common.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
   $('#title').text('홈');
-
-  $('.aaaa').click(function() {
-    let linkTitle = $(this).attr('title'); // 클릭한 a 태그의 title 속성 값을 가져옴
-    $('#title').text(linkTitle); // p 태그의 텍스트 내용을 가져온 title 속성 값으로 변경
-    
-    // 모든 a 태그의 background 색상 초기화
-    $('.aaaa').css('background-color', 'transparent');
-    // 클릭한 a 태그의 background 색상 변경
-    $(this).css('background-color', '#9DADC7');
-  });
+  
+  let gnb_menu = document.querySelectorAll('.header-menu li');
+  let url = window.location.href;
+  url = url.split("/");
+  
+  if(url[6] == "board"){
+    gnb_menu[3].classList.add('on');
+  }
 });
+
