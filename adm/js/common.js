fc73bb1c530cb2ce62e1df4df56915a02d09b028
chadonghyun <121537101+chadonghyun@users.noreply.github.com>
2023-04-17 12:11:06 +0900

    PETxLAB ver 1.0.4
    PETxLAB ver 1.0.4

--- adm/js/common.js
+++ adm/js/common.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
   $('#title').text('홈');
   
-  let gnb_menu = document.querySelectorAll('.header-menu li');
+  let gnb_menu = $('.header-menu li');
   let url = window.location.href;
   url = url.split("/");
-  
-  if(url[6] == "board"){
-    gnb_menu[3].classList.add('on');
+  let page = url[6];
+
+  if(page == "board"){
+    $('#title').text('게시판관리')
+    gnb_menu.eq(3).addClass('on')
+  }else if(page == "lecture"){
+    $('#title').text('강의관리')
+    gnb_menu.eq(2).addClass('on')
+  }else if(page == "a_index.php" || page == "t_index.php"){
+    $('#title').text('홈')
+    gnb_menu.eq(0).addClass('on')
+  }else if(page == "member"){
+    $('#title').text('회원관리')
+    gnb_menu.eq(1).addClass('on')
   }
 });
-
