fd03e35f54de7bba46a7baeb8c8449369b8e6db1
mypiay123 <mypiay123@gmail.com>
2023-05-17 19:34:59 +0900

    ver 2.1.45
    Update update_confirm.php

--- adm/admin/confirm/update_confirm.php
+++ adm/admin/confirm/update_confirm.php
@@ -4,12 +4,22 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/PETxLAB/db/db_con.php';
 include_once $_SERVER['DOCUMENT_ROOT']."/PETxLAB/config.php";
 
 $user_course_id = $_POST['user_course_id'];
-
-// echo $user_course_id;
-
-
-$sql = "UPDATE user_course SET confirm = 1 WHERE user_course_id = '$user_course_id'";
+$sql = "UPDATE user_course SET confirm = 1 and status = 'in_progress' WHERE user_course_id = '$user_course_id'";
 $result = mysqli_query($con, $sql);
+
+$sql2 = "SELECT * FROM user_course 
+        JOIN video
+        on user_course.course_id = video.course_id
+        WHERE user_course_id = '$user_course_id'";
+$result2 = mysqli_query($con, $sql2);
+
+while($row = mysqli_fetch_array($result2)){
+    $course_id = $row['course_id'];
+    $video_id = $row['video_id'];
+
+    $video_insert = "INSERT INTO video_progress(course_id, video_id, user_id, video_status) VALUES('$course_id', '$video_id', '$userid', 0)";
+    $insert_result = mysqli_query($con, $video_insert);
+};
 
 
 if($result){
